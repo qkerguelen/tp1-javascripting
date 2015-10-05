1d3f9b282b0d6a3ce16638af473d846448f74716
Quentin Kerguélen <q.kerguelen@outlook.com>
2015-10-05 10:40:28 +0200

    Premier commit

---
+++ javascripting/function-arguements.js
@@ -0,0 +1,7 @@
+function math (x, y, z) {
+  var multiplied = y*z;
+  var result = multiplied + x;
+  return result;
+}
+
+console.log(math(53,61,67));
---
+++ javascripting/functions.js
@@ -0,0 +1,7 @@
+var food = "This pizza";
+
+function eat (food) {
+  return food + ' tasted really good.';
+}
+
+console.log(eat(food));
